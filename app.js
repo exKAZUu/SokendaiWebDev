6624ea645406ad50ba2919ffd10899066b0e02b5
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-10-17 04:55:20 +0900

    Use static files instead of writing string.

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-var http = require('http');
-var port = process.env.PORT || 3000;
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('\
-<!DOCTYPE html>\n\
-<html>\n\
-  <head>\n\
-    <title>Hello</title>\n\
-  </head>\n\
-  <body>\n\
-    <h1>Hello</h1> World!\n\
-  </body>\n\
-</html>\
-  ');
-}).listen(port);
-console.log('Server running at http://127.0.0.1:' + port + '/');
+var connect = require('connect')
+var serveStatic = require('serve-static')
+
+var app = connect()
+app.use(serveStatic('static'))
+app.listen(3000)
